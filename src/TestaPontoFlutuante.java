17e3c158aa08b999326106b088f015315a170f55
thpereira90 <thi_per@hotmail.com>
2022-09-03 16:17:32 -0300

    Testa ponto Flutuante

---
+++ src/TestaPontoFlutuante.java
@@ -0,0 +1,20 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[] args) {
+	
+		double salario = 1250.70;
+		
+		System.out.println(salario);
+		
+		System.out.println("Meu Salário é "+ salario);
+	
+		//NÃO PODEMOS COLOCAR UM VALOR COM PONTO FLUTUANTE DENTRO DE UMA VARIAVEL DO TIPO INT
+		
+		double divisao = 5.0 / 2;
+		
+		System.out.println(divisao);
+		
+	}
+	
+}
