363aea7817fb2c7ab5a5eee2f7aa5e5d954e616e
thpereira90 <thi_per@hotmail.com>
2022-09-03 15:51:36 -0300

    First commit

---
+++ src/testaVariaveis.java
@@ -0,0 +1,14 @@
+
+public class testaVariaveis {
+	
+	public static void main(String [] args) {
+		
+		System.out.println("olá novo teste");
+		
+		int idade = 37;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é "+ idade + ", Parabéns!");
+	}
+}
